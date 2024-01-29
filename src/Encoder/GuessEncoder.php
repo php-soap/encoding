6a40f03b2ecf7007045ee4541800437284330fda
Toon Verwerft <toonverwerft@gmail.com>
2024-01-29 07:11:22 +0100

    Add todo

--- src/Encoder/GuessEncoder.php
+++ src/Encoder/GuessEncoder.php
@@ -16,6 +16,8 @@ class GuessEncoder implements XmlEncoder
         $meta = $type->getMeta();
 
         $encoder =  match (true) {
+            // TODO : List / Nullable should be wrapped in a different location.
+            // TODO : otherwise, it only works when guessed and not when fetched by type directly from the registry.
             $meta->isList()->unwrapOr(false) => new ListEncoder(),
             $meta->isSimple()->unwrapOr(false) => new ExtendedSimpleTypeEncoder(),
             default => new ObjectEncoder(\stdClass::class)
