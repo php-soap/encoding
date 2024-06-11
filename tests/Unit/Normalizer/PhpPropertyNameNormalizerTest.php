925a842161ef2d79f705010a6d02511f714a5aa9
Toon Verwerft <toonverwerft@gmail.com>
2024-06-11 14:11:36 +0200

    Normalize property names to supported php names in the object encoder

---
+++ tests/Unit/Normalizer/PhpPropertyNameNormalizerTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Soap\Encoding\Test\Unit\Normalizer;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Soap\Encoding\Normalizer\PhpPropertyNameNormalizer;
+
+#[CoversClass(PhpPropertyNameNormalizer::class)]
+final class PhpPropertyNameNormalizerTest extends TestCase
+{
+
+    /**
+     *
+     * @dataProvider provideCases
+     */
+    public function test_it_can_normalize(string $in, string $expected): void
+    {
+        static::assertEquals($expected, PhpPropertyNameNormalizer::normalize($in));
+    }
+
+    public static function provideCases()
+    {
+        yield ['prop1', 'prop1'];
+        yield ['final', 'final'];
+        yield ['Final', 'Final'];
+        yield ['UpperCased', 'UpperCased'];
+        yield ['my-./*prop_123', 'myProp_123'];
+        yield ['My-./*prop_123', 'MyProp_123'];
+        yield ['My-./final*prop_123', 'MyFinalProp_123'];
+    }
+}
